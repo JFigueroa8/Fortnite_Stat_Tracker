aa4f4d4743b2c1c5b8834852201b3e070266a2e5
Julmar <julmarfigueroa@gmail.com>
2019-06-14 17:50:41 -0400

    Changed to template literals

--- static/fortnitetracker.js
+++ static/fortnitetracker.js
@@ -5,7 +5,7 @@ $(function() {
     let results = $('#results');
 
     // default values
-    let dropDownValue = 'pc';
+    let dropDownValue = 'xbl';
 
     submitBtn.click(function() {
         let data = {};
@@ -35,19 +35,33 @@ $(function() {
 
     function displayData(data) {
         let epicUserHandle = data.epicUserHandle;
-        let list =  '<ul class="list-group">' + 
-                        '<li class="list-group-item">' + 'Solo: ' + data.stats.p2.top1.value + '</li>' +
-                        '<li class="list-group-item">' + 'Duos: ' + data.stats.p10.top1.value + '</li>' +
-                        '<li class="list-group-item">' + 'Teams: ' + data.stats.p9.top1.value + '</li>' +
-                    '</ul>';
+        let list =  `<ul class="list-group"> 
+                        <li class="list-group-item">Solo:  ${data.stats.p2.top1.value} --- Kills: ${data.stats.p2.kills.value}</li>
+                        <li class="list-group-item">Duos:  ${data.stats.p10.top1.value} --- Kills: ${data.stats.p10.kills.value}</li>
+                        <li class="list-group-item">Teams:  ${data.stats.p9.top1.value} --- Kills: ${data.stats.p9.kills.value}</li>
+                    </ul>`;
 
-        let template = '<div class="card text-center">' + 
-                            '<h5 class="card-header">' + epicUserHandle + '</h5>' +
-                            '<div class="card-body">' + 
-                            '<h5 class="card-title">' + 'Wins' + '</h5>' +
-                            '<p class="card-text">' + list + '</p>' +
-                            '</div>' +
-                        '</div>';
+        let template = `<div class="card text-center"> 
+                            <h5 class="card-header">${epicUserHandle}</h5>
+                            <div class="card-body"> 
+                                <h5 class="card-title">Wins</h5>
+                                <p class="card-text">${list}</p>
+                            </div>
+                        </div>`;
+        // let list =  '<ul class="list-group">' + 
+        //                 '<li class="list-group-item">' + 'Solo: ' + data.stats.p2.top1.value + '</li>' +
+        //                 '<li class="list-group-item">' + 'Duos: ' + data.stats.p10.top1.value + '</li>' +
+        //                 '<li class="list-group-item">' + 'Teams: ' + data.stats.p9.top1.value + '</li>' +
+        //             '</ul>';
+
+        
+        // let template = '<div class="card text-center">' + 
+        //                     '<h5 class="card-header">' + epicUserHandle + '</h5>' +
+        //                     '<div class="card-body">' + 
+        //                     '<h5 class="card-title">' + 'Wins' + '</h5>' +
+        //                     '<p class="card-text">' + list + '</p>' +
+        //                     '</div>' +
+        //                 '</div>';
 
         results.html(template);
     }
